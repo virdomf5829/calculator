d9f2b295fe958900adcbd2ef31cd3cfc8ceabe60
virdomf5829 <virdomf261104@students.amikom.ac.id>
2024-03-05 12:16:35 +0700

    calculator26.cs

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,23 @@ internal class program
 {
 	static void main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		console.writeline("hasil penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+		console.writeline("hasil pengurangan: {0} - {1} = {2}", a, b, pengurangan(a, b));
+		
+		console.writeline("\nTekan sembarang key untuk keluar");
+		console.readkey();
+	}
+	
+	static int penambahan(int a, int b)
+	{
+		return a = b;
+	}
+	
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
